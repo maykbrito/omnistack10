dbba6cf0d215b1c3d6c638d33c79a9c6463b8f7b
Mayk Brito <maykbrito@gmail.com>
2020-01-16 16:08:06 -0400

    chore: add main content, styling, responsive

--- web/src/App.js
+++ web/src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import './global.css';
 import './App.css';
 import './Sidebar.css';
+import './Main.css';
+import './responsive.css';
 
 function App() {
   return (
@@ -36,7 +38,52 @@ function App() {
         </form>
       </aside>
       <main>
-
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://placehold.it/500" alt="Imagem de"/>
+              <div className="user-info">
+                <strong>Mayk Brito</strong>
+                <span>Javascript, HTML, CSS</span>
+              </div>
+            </header>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Arum quae officia modi consequatur vero eum delectus voluptatibus!</p>
+            <a href="https://github.com/maykbrito">Acessar Perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://placehold.it/500" alt="Imagem de"/>
+              <div className="user-info">
+                <strong>Mayk Brito</strong>
+                <span>Javascript, HTML, CSS</span>
+              </div>
+            </header>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Arum quae officia modi consequatur vero eum delectus voluptatibus!</p>
+            <a href="https://github.com/maykbrito">Acessar Perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://placehold.it/500" alt="Imagem de"/>
+              <div className="user-info">
+                <strong>Mayk Brito</strong>
+                <span>Javascript, HTML, CSS</span>
+              </div>
+            </header>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Arum quae officia modi consequatur vero eum delectus voluptatibus!</p>
+            <a href="https://github.com/maykbrito">Acessar Perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://placehold.it/500" alt="Imagem de"/>
+              <div className="user-info">
+                <strong>Mayk Brito</strong>
+                <span>Javascript, HTML, CSS</span>
+              </div>
+            </header>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Arum quae officia modi consequatur vero eum delectus voluptatibus!</p>
+            <a href="https://github.com/maykbrito">Acessar Perfil no Github</a>
+          </li>
+        </ul>
       </main>
     </div>
   );
