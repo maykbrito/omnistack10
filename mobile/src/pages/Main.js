e8579856041a386e458c795bf360a5b36aa086a0
Mayk Brito <maykbrito@gmail.com>
2020-01-19 14:03:15 -0400

    chore: connect socket with backend

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -5,6 +5,7 @@ import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 import { MaterialIcons } from '@expo/vector-icons'
 
 import api from '../services/api'
+import { connect, disconnect } from '../services/socket'
 
 export default function Main({ navigation }) {
   const [devs, setDevs] = useState([])
@@ -34,6 +35,10 @@ export default function Main({ navigation }) {
     loadInitialPosition()
   }, [])
 
+  function setupWebsocket() {
+    connect();
+  }
+
   async function loadDevs() {
     const { latitude, longitude } = currentRegion;
 
@@ -46,6 +51,7 @@ export default function Main({ navigation }) {
     })
 
     setDevs(response.data)
+    setupWebsocket()
   }
 
   function handleRegionChanged(region) {
