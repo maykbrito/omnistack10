a7e4f7440e72fb6f190fa50060f398000dff2db3
Mayk Brito <maykbrito@gmail.com>
2020-01-18 22:49:59 -0400

    chore: add expo location and current phone location

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet } from 'react-native';
 import MapView from 'react-native-maps';
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+
 
 export default function Main() {
-  return (
-    <MapView style={styles.map}/>
-  );
+  const [currentRegion, setCurrentRegion] = useState(null)
+
+  useEffect(() => {
+    async function loadInitialPosition() {
+      const { granted } = await requestPermissionsAsync();
+
+      if (granted) {
+        const { coords } = await getCurrentPositionAsync({
+          enableHighAccuracy: true, //needs gps enabled
+        })
+
+        const { latitude, longitude } = coords;
+
+        setCurrentRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 0.04,
+          longitudeDelta: 0.04
+        })
+      }
+    }
+
+    loadInitialPosition()
+  }, [])
+
+  if (!currentRegion) 
+    return null
+
+  return <MapView style={styles.map} initialRegion={currentRegion}/>
 }
 
 const styles = StyleSheet.create({
