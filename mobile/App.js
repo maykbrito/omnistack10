0fd8b79ee2c46d7afe768c8d5dd19362f76b6b2c
Mayk Brito <maykbrito@gmail.com>
2020-01-18 22:24:50 -0400

    chore: add dependencies

--- mobile/App.js
+++ mobile/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Valeska</Text>
     </View>
   );
 }
@@ -12,8 +12,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#71df9c',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: '#FFF',
+    fontSize: 32
+  }
 });
