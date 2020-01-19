d8077cce4e6a0c077a9faffd17eb1d872f4cae87
Mayk Brito <maykbrito@gmail.com>
2020-01-18 22:36:14 -0400

    chore: change app title

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -6,8 +6,18 @@ import Profile from './pages/Profile'
 
 const Routes = createAppContainer(
   createStackNavigator({
-    Main,
-    Profile
+    Main: {
+      screen: Main,
+      navigationOptions: {
+        title: 'DevRadar'
+      }
+    },
+    Profile: {
+      screen: Profile,
+      navigationOptions: {
+        title: 'Perfil no Github'
+      }
+    }
   })
 )
 
