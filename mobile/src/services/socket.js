c0c2e12dee5941aa434b5350037fdccb03fd73db
Mayk Brito <maykbrito@gmail.com>
2020-01-19 13:59:58 -0400

    chore: configure socket.io

---
+++ mobile/src/services/socket.js
@@ -0,0 +1,19 @@
+import socketio from 'socket.io-client'
+
+const socket = socketio('http://192.168.0.102:3333', {
+  autoConnect: false
+})
+
+function connect() {
+  socket.connect()
+}
+
+function disconnect(){
+  if (socket.connected)
+    socket.disconnect()
+}
+
+export {
+  connect,
+  disconnect,
+}
