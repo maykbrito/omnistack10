8b818ff37631dfd9f211f6c504e4ce163f04cfda
Mayk Brito <maykbrito@gmail.com>
2020-01-19 13:38:18 -0400

    chore: extract http from server

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,9 +2,11 @@ const { MONGO_USER, MONGO_PASS, MONGO_DB } = require('../.env')
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require('cors');
+const http = require('http')
 const routes = require("./routes");
 
 const app = express();
+const server = http.Server(app)
 
 mongoose.connect(
   `mongodb+srv://${MONGO_USER}:${MONGO_PASS}@cluster0-6ij0n.mongodb.net/${MONGO_DB}?retryWrites=true&w=majority`,
@@ -20,4 +22,4 @@ app.use(cors())
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
