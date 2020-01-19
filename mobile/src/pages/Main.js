c889253f926e3f64aa9786629db122f8faa81458
Mayk Brito <maykbrito@gmail.com>
2020-01-18 22:43:24 -0400

    refactor: maps styles

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
 import MapView from 'react-native-maps';
 
 export default function Main() {
   return (
-    <MapView style={{ flex: 1 }}/>
+    <MapView style={styles.map}/>
   );
 }
+
+const styles = StyleSheet.create({
+  map: { flex: 1 }
+})
