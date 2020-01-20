5bb9492c1cfb0f2b4895d99c38ee7de1b5343341
Mayk Brito <maykbrito@gmail.com>
2020-01-19 22:24:41 -0400

    feat: socket.io sendMessage from backend

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const Dev = require("../models/Dev");
 const parseStringAsArray = require("../utils/parseStringAsArray");
-const { findConnections } = require('../websocket')
+const { findConnections, sendMessage } = require('../websocket')
 module.exports = {
   async index(req, res) {
     const devs = await Dev.find();
@@ -44,7 +44,7 @@ module.exports = {
         techsArray,
       )
 
-      console.log(sendSocketMessageTo)
+      sendMessage(sendSocketMessageTo, 'new-dev', dev)
     }
 
     return res.json(dev);
--- backend/src/websocket.js
+++ backend/src/websocket.js
@@ -2,10 +2,11 @@ const socketio = require('socket.io')
 const parseStringAsArray = require('./utils/parseStringAsArray')
 const calculateDistance = require('./utils/calculateDistance')
 
+let io;
 const connections = []
 
 exports.setupWebsocket = (server) => {
-  const io = socketio(server)
+  io = socketio(server)
 
   io.on('connection', socket => {
     const { latitude, longitude, techs } = socket.handshake.query;
@@ -25,5 +26,24 @@ exports.findConnections = (coordinates, techs) => {
   return connections.filter(connection => { 
     return calculateDistance(coordinates, connection.coordinates) < 10 //10km
       && connection.techs.some(item => techs.includes(item))
+  })
+}
+
+// Exemple of to
+// [
+//   {
+//     id: 'WR2qBMPmC9jKaKU_AAAA',
+//     coordinates: { latitude: -20.42925087049043, longitude: -54.578255731612444 },
+//     techs: [ 'Node.js' ]
+//   },
+//   {
+//     id: 'xhNlanox8wYlgrSRAAAB',
+//     coordinates: { latitude: -20.431115044003583, longitude: -54.57948284223676 },
+//     techs: [ 'Node.js' ]
+//   }
+// ]
+exports.sendMessage = (to, message, data) => {
+  to.forEach(connection => {
+    io.to(connection.id).emit(message, data)
   })
 }
