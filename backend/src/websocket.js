7c86fd5085b953e0812e5e98c683a816ddb5079a
Mayk Brito <maykbrito@gmail.com>
2020-01-19 21:09:42 -0400

    chore: store websocket connections

--- backend/src/websocket.js
+++ backend/src/websocket.js
@@ -1,11 +1,21 @@
 const socketio = require('socket.io')
+const parseStringAsArray = require('./utils/parseStringAsArray')
+
+const connections = []
 
 exports.setupWebsocket = (server) => {
   const io = socketio(server)
 
   io.on('connection', socket => {
-    setTimeout(() => {
-      socket.emit('message', 'Sending message from backend')
-    }, 3000)
+    const { latitude, longitude, techs } = socket.handshake.query;
+
+    connections.push({
+      id: socket.id,
+      coordinates: {
+        latitude: Number(latitude),
+        longitude: Number(longitude),
+      },
+      techs: parseStringAsArray(techs),
+    })
   })
 }
